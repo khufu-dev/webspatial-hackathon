db323f6748351aa4969658a333cc1f18d6f6af7b
Siyaman <siyaman.sadagopan@bytedance.com>
2025-12-11 17:53:48 -0800

    Add support for light and dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import MainPage from './MainPage'
 
 // Enable spatial window styling
 const html = document.documentElement;
 html.classList.add('is-spatial');
-html.style.setProperty('--xr-background-material', 'translucent');
-html.style.backgroundColor = 'transparent';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
