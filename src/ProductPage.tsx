b312ed7380d7e15409716a7a5c9a030ae5389c44
evan.mickelson <evan.mickelson@bytedance.com>
2025-12-12 11:31:32 -0800

    rotating model

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -1,6 +1,15 @@
 import { Link, useParams } from "react-router-dom";
 import { products } from "./data/products";
 import "./App.css";
+import {
+  BoxEntity,
+  ModelAsset,
+  ModelEntity,
+  Reality,
+  SceneGraph,
+  UnlitMaterial,
+} from "@webspatial/react-sdk";
+import { useEffect, useState } from "react";
 
 export default function ProductPage() {
   const { id } = useParams<{ id: string }>();
@@ -16,21 +25,37 @@ export default function ProductPage() {
     );
   }
 
+  const [rotation, setRotation] = useState({ x: 0, y: 0, z: 0 });
+  useEffect(() => {
+    let id: number;
+    function animate() {
+      setRotation((prev) => ({ ...prev, y: prev.y + 1.0 }));
+      id = requestAnimationFrame(animate);
+    }
+    animate();
+    return () => cancelAnimationFrame(id);
+  }, []);
+
   //const modelUrl = (product as any).modelUrl as string | undefined;
 
   return (
     <div className="app">
       <div style={{ maxWidth: 720, margin: "0 auto" }}>
-        <img
-          src={product.image}
-          alt={product.name}
-          className="product-image"
-          style={{ width: "100%", height: "auto" }}
-        />
-
         <h1 className="product-name" style={{ marginTop: "1rem" }}>
           {product.name}
         </h1>
+        <Reality style={{ width: "500px", height: "500px", "--xr-depth": 100 }}>
+          <UnlitMaterial id="red" color="#ff0000" />
+          <SceneGraph>
+            <BoxEntity
+              materials={["red"]}
+              width={0.1}
+              height={0.2}
+              depth={0.1}
+              rotation={rotation}
+            />
+          </SceneGraph>
+        </Reality>
         <p className="product-price">
           ${product.price.toFixed(2)}
         </p>
