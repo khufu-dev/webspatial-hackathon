df74b6f0bcccb47d1f735421c74ec1da4e515f20
Siyaman <siyaman.sadagopan@bytedance.com>
2025-12-12 16:49:51 -0800

    Remove SecondPage

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <BrowserRouter basename={__XR_ENV_BASE__}>
       <Routes>
-        {/* Add a root route to prevent blank screen */}
         <Route path="/" element={<MainPage />} />
-        {/* <Route path="/second-page" element={<SecondPage />} /> */}
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </BrowserRouter>
