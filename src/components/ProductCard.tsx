7fa67a12ef021158964044a3154a6bf50906aaef
Siyaman <siyaman.sadagopan@bytedance.com>
2025-12-16 16:04:50 -0800

    Add Model3D component that encompasses 3D model rendering logic
    Different environments require different code to render 3D models

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,6 +4,7 @@ import {
   ModelEntity,
   Reality,
   SceneGraph,
+  Vec3,
 } from "@webspatial/react-sdk";
 import { Product } from "../data/products";
 import "./ProductCart.css";
@@ -15,20 +16,7 @@ interface ProductCardProps {
 export function ProductCard({ product }: ProductCardProps) {
   return (
     <div className="product-card">
-      <Reality style={{ width: "100px", height: "200px", "--xr-depth": 100 }}>
-        <ModelAsset
-          id={product.model}
-          src={`${import.meta.env.BASE_URL}${product.model}`}
-        />
-        <SceneGraph>
-          <ModelEntity
-            model={product.model}
-            position={product.position}
-            scale={product.scale}
-            rotation={product.rotation}
-          />
-        </SceneGraph>
-      </Reality>
+      <Model3D className="product-3D" {...product} src={product.model} />
       <h3 className="product-name">{product.name}</h3>
       <p className="product-description">{product.description}</p>
       <p className="product-price">${product.price.toFixed(2)}</p>
@@ -38,5 +26,28 @@ export function ProductCard({ product }: ProductCardProps) {
         </Link>
       </div>
     </div>
+  );
+}
+
+type Model3DProps = {
+  src: string;
+  position: Vec3;
+  scale: Vec3;
+  rotation: Vec3;
+  className?: string
+}
+function Model3D({ className, src, position, scale, rotation }: Model3DProps) {
+  return (
+    <Reality className={className}>
+      <ModelAsset id={src} src={src} />
+      <SceneGraph>
+        <ModelEntity
+          model={src}
+          position={position}
+          scale={scale}
+          rotation={rotation}
+        />
+      </SceneGraph>
+    </Reality>
   );
 }
