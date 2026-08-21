0f9f01f2ca858e9b297a0f8aa8bef9bcdfd25d3d
Siyaman <siyaman.sadagopan@bytedance.com>
2026-08-21 15:50:19 -0700

    Upgrade to WebSpatial SDK 2.0
    WebSpatial 2.0 introduced the breaking change of using SpatialBoot

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import MainPage from "./MainPage";
 import ProductPage from "./ProductPage";
+import { SpatialBoot } from "@webspatial/react-sdk";
 
 export default function Router() {
   return (
     <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-        </Routes>
-      </BrowserRouter>
+      <SpatialBoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+        </BrowserRouter>
+      </SpatialBoot>
     </StrictMode>
   );
 }
